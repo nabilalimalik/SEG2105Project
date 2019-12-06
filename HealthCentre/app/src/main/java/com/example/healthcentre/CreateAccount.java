65978fdead73d483d360f2ea1c74b7c29fc1e6ed
nabilalimalik <nabilalimalik@gmail.com>
2019-12-06 18:55:24 -0500

    Updated app to create accounts

--- HealthCentre/app/src/main/java/com/example/healthcentre/CreateAccount.java
+++ HealthCentre/app/src/main/java/com/example/healthcentre/CreateAccount.java
@@ -1,17 +1,85 @@
 package com.example.healthcentre;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
+
+    private EditText setfirstname, setlastname, setusername, setpassword;
+    private Button setcreateaccount;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        setupUIViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        setcreateaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()) {
+                    //Upload data to the database
+                    String add_firstname = setfirstname.getText().toString().trim();
+                    String add_lastname = setlastname.getText().toString().trim();
+                    String add_username = setusername.getText().toString().trim();
+                    String add_password = setpassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(add_username, add_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(CreateAccount.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(CreateAccount.this, MainActivity.class));
+                            }
+                            else{
+                                Toast.makeText(CreateAccount.this, "Registration Unsuccessful", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        setfirstname = (EditText)findViewById(R.id.firstname);
+        setlastname = (EditText)findViewById(R.id.lastname);
+        setusername = (EditText)findViewById(R.id.username);
+        setpassword = (EditText)findViewById(R.id.password);
+        setcreateaccount = (Button)findViewById(R.id.createaccount);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+
+        String firstnameCheck = setfirstname.getText().toString();
+        String lastnameCheck = setlastname.getText().toString();
+        String usernameCheck = setusername.getText().toString();
+        String passwordCheck = setpassword.getText().toString();
+
+        if(usernameCheck.isEmpty() || passwordCheck.isEmpty()){
+            Toast.makeText(this, "Please enter all information", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            result = true;
+        }
+
+        return result;
     }
 
     public void AlreadyCreated(View view) {
--- HealthCentre/app/src/main/java/com/example/healthcentre/MainActivity.java
+++ HealthCentre/app/src/main/java/com/example/healthcentre/MainActivity.java
@@ -1,22 +1,51 @@
 package com.example.healthcentre;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText usernameCheck ,passwordCheck;
+    EditText username ,password;
+    Button loginButton;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        passwordCheck = (EditText)findViewById(R.id.password);
-        usernameCheck = (EditText)findViewById(R.id.username);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        password = (EditText)findViewById(R.id.password);
+        username = (EditText)findViewById(R.id.username);
+        loginButton =  (Button)findViewById(R.id.login);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //if(user != null){
+        //    finish();
+        //    startActivity(new Intent(MainActivity.this, Patient.class));
+        //}
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                login(username.getText().toString(), password.getText().toString());
+            }
+        });
     }
 
     public void NewPatientAccount(View view) {
@@ -31,8 +60,29 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult (intent,0);
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(getApplicationContext(), Welcome.class);
-        startActivityForResult (intent,0);
+    public void login(final String usernameCheck, final String passwordCheck) {
+
+
+        progressDialog.setMessage("Logging in. Please wait.");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(usernameCheck, passwordCheck).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this, Patient.class));
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+                else if ((usernameCheck.equals("admin"))  && (passwordCheck.equals("5T5ptQ"))) {
+                    finish();
+                    Intent intent = new Intent(getApplicationContext(), Admin.class);
+                    startActivityForResult(intent, 0);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Username / Password combination does not exist.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
